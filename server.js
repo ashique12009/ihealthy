3b28e8bcb828c5c055d9aead18d4cdf1f5b22e60
Ashique <ashique12009@gmail.com>
2023-12-29 22:49:14 +0200

    mongodb connection string is good to go

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
 const app = express();
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
-app.listen(3000, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
