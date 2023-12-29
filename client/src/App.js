000b912445a901c3a89ad6bc70c7983c0efa2271
Ashique <ashique12009@gmail.com>
2023-12-29 23:22:47 +0200

    few client setup stuff route related stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
+import { DatePicker } from 'antd';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
   return (
-    <div className="App">
-      <h1>iHealthy</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
